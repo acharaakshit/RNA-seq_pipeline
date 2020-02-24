acd4ba784b16957c2fd9c7ebc45618f22578f771
acharaakshit <acharaakshit@gmail.com>
2020-02-25 02:20:32 +0530

    initial commit

---
+++ ballgown_script.R
@@ -0,0 +1,11 @@
+library(ballgown)
+
+#brain 
+bg_brain <- ballgown(samples = "~/brain_output" , meas = 'all')
+brain_fpkm_mat <- texpr(bg_brain, meas = "FPKM")
+write.csv(brain_fpkm_mat , "brain_fpkm.csv", row.names = FALSE)
+
+#liver
+bg_liver <- ballgown(samples = "~/liver_output", meas = "all")
+liver_fpkm_mat <- texpr(bg_liver, meas = "FPKM")
+write.csv(liver_fpkm_mat,"liver_fpkm.csv", row.names = FALSE)
