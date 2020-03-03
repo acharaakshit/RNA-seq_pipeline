dddf11cf24e189af364eb2ae828522924f000aea
acharaakshit <acharaakshit@gmail.com>
2020-03-03 23:26:29 +0530

    updated the bash script(that uses tablemaker) to produce merged gtfs

--- bash_script_w_tablemaker.sh
+++ bash_script_w_tablemaker.sh
@@ -3,5 +3,8 @@ eval "stringtie -L ~/RNA_nanopore.brain.C1R1_mapping_E94_minimap2_primary_no_rea
 eval "tail -n +1 ~/brain.gtf | head -n 1001 > ~/first1000_brain.gtf"
 eval "stringtie -L ~/RNA_nanopore.liver.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC_genome_convert.sorted.bam >> liver.gtf"
 eval "tail -n +1 ~/liver.gtf | head -n 1001 > ~/first1000_liver.gtf"
-eval "tablemaker -p 3 -q -W -G first1000_brain.gtf -o ~/brain_output ~/RNA_nanopore.brain.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC_genome_convert.sorted.bam"
-eval "tablemaker -p 3 -q -W -G first1000_liver.gtf -o ~/liver_output ~/RNA_nanopore.liver.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC_genome_convert.sorted.bam"
+eval "stringtie --merge first1000_brain.gtf -o merged_brain.gtf"
+eval "stringtie --merge first1000_liver.gtf -o merged_liver.gtf"
+
+eval "tablemaker -p 3 -q -W -G merged_brain.gtf -o ~/brain_output ~/RNA_nanopore.brain.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC_genome_convert.sorted.bam"
+eval "tablemaker -p 3 -q -W -G merged_liver.gtf -o ~/liver_output ~/RNA_nanopore.liver.C1R1_mapping_E94_minimap2_primary_no_read_less_than_80QC_genome_convert.sorted.bam"
